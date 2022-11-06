d865d43f7769131ce6c77b24cfafade2256ab2ac
unknown <saeedahmad.icp@gmail.com>
2022-11-06 15:40:00 +0900

    Image segmentation

---
+++ Image Segmentation/dataset.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 10:02:04 2022
+
+@author: Saeed Ahmad
+"""
+
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+from torchvision import transforms as t
+from torch.utils.data import DataLoader
+
+TRAIN_IMG_DIR = "./data/train/"
+TRAIN_MASK_DIR = "./data/train_masks/"
+IMAGE_HEIGHT = 160  # 1280 originally
+IMAGE_WIDTH = 240  # 1918 originally
+
+class CarvanaDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None, transform_masks=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.transform_mask = transform_masks
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        #print(self.image_dir, "mask directory: ", self.images[0][index]) 
+        img_path = os.path.join(self.image_dir, str(self.images[index]))
+        mask_path = os.path.join(self.mask_dir, str(self.images[index]).replace(".jpg", "_mask.gif"))
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"))
+        mask[mask == 255.0] = 1.0
+
+      
+
+        if self.transform is not None:
+            image = self.transform(image)
+            mask = self.transform_mask(mask)
+           # augmentations = self.transform(image=image, mask=mask)
+           # image = augmentations["image"]
+           # mask = augmentations["mask"]
+
+        return image, mask
+
+
+
+
+
+
+
+
---
+++ Image Segmentation/train.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 10:02:05 2022
+
+@author: Saeed Ahmad
+"""
+
+import torch
+#from albumentations.pytorch import ToTensorV2
+from torchvision import transforms as t
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import U_NET
+
+
+
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imgs,
+)
+
+
+# Hyperparameters etc.
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+IMAGE_HEIGHT = 160  # 1280 originally
+IMAGE_WIDTH = 240  # 1918 originally
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "./data/train/"
+TRAIN_MASK_DIR = "./data/train_masks/"
+
+
+
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    #loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loader):
+        if batch_idx < 300:
+            data = data.to(device=DEVICE)
+            targets = targets.float().to(device=DEVICE)
+
+            # forward
+            with torch.cuda.amp.autocast():
+                predictions = model(data)
+                #print("Prediction shape: ", predictions.shape)
+                loss = loss_fn(predictions, targets)
+
+            # backward
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            # update tqdm loop
+            #loader.set_postfix(loss=loss.item())
+
+
+
+def main():
+    train_transform = t.Compose(
+        [
+            t.ToPILImage(),
+            t.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+            #A.Rotate(limit=35, p=1.0),
+           # A.HorizontalFlip(p=0.5),
+            #A.VerticalFlip(p=0.1),
+            t.ToTensor(),
+            t.Normalize(
+            mean=[0.0, 0.0, 0.0],
+            std=[1.0, 1.0, 1.0],
+           # max_pixel_value=255.0,
+        )
+           ,
+            
+        ],
+    )
+
+    train_transform_masks = t.Compose(
+        [
+            t.ToPILImage(),
+            t.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+            #A.Rotate(limit=35, p=1.0),
+           # A.HorizontalFlip(p=0.5),
+            #A.VerticalFlip(p=0.1),
+            t.ToTensor(),
+            
+        ],
+    )
+
+
+    
+    
+
+    model = U_NET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+    train_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        train_transform_masks,
+    )
+
+
+    if LOAD_MODEL:
+        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+
+
+    check_accuracy(train_loader, model, device=DEVICE)
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(NUM_EPOCHS):
+        print("Training")
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        print("testing")
+
+        # save model
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer":optimizer.state_dict(),
+        }
+        save_checkpoint(checkpoint)
+
+        # check accuracy
+        check_accuracy(train_loader, model, device=DEVICE)
+
+        # print some examples to a folder
+        #save_predictions_as_imgs(
+          #  train_loader, model, folder="saved_images/", device=DEVICE
+       # )
+
+
+if __name__ == "__main__":
+    main()
